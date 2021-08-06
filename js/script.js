420c53d2a2f06c4cc87f00bc345619416f240d74
BenilDavid <benil.26david@gmail.com>
2021-08-06 20:19:57 +0530

    sorting and range slider

--- js/script.js
+++ js/script.js
@@ -66,6 +66,7 @@ var productsArray = [
 
 window.onload = function () {
 	renderProducts(productsArray);
+	renderTopProducts(topProductsArray);
 };
 
 function renderProducts(arr) {
@@ -96,33 +97,178 @@ function renderProducts(arr) {
 		productCard.innerHTML = productCardDetails;
 	});
 }
+var topProductsArray = [
+	{
+		id: 1,
+		productCategory: 'Misc',
+		productTitle: 'Mug Mockup',
+		price: 99.0,
+		topProducts: true,
+		uploadedImageSrc: 'https://source.unsplash.com/70x70/?mug,mockup',
+	},
+];
+function renderTopProducts(toparr) {
+	var topProductsShowContainer = document.querySelector(
+		'.mini-product-show-container'
+	);
+
+	toparr.map((e) => {
+		var topProductsShow = document.createElement('div');
+		topProductsShow.setAttribute('class', 'mini-product-show');
+		topProductsShowContainer.appendChild(topProductsShow);
+		let topProductDetails = `
+                            <img src="${e.uploadedImageSrc}" alt="" />
+							<div class="product-content">
+								<span class="mini-product-heading">${e.productTitle}</span>
+								<div class="star">
+									<img src="images/star.png" alt="" />
+									<img src="images/star.png" alt="" />
+									<img src="images/star.png" alt="" />
+									<img src="images/star.png" alt="" />
+									<img src="images/star.png" alt="" />
+								</div>
+								<div class="mini-price">$${e.price}</div>
+							</div>
+        `;
+		topProductsShow.innerHTML = topProductDetails;
+	});
+}
 
 const addProduct = () => {
-	productsArray = [];
-	alert('saved');
+	// productsArray = [];
+	// topProductsArray = [];
+	// alert('saved');
 	obj = {};
 	var e = document.getElementById('selectedCategory');
 	var category = e.options[e.selectedIndex].text;
 	obj['productCategory'] = category;
-	console.log(category);
 	var title = document.getElementById('title').value;
 	obj['productTitle'] = title;
-	console.log(title);
 	var price = document.getElementById('price').value;
 	obj['price'] = price;
-	console.log(price);
 	var topProduct = document.getElementById('topProduct').checked;
 	obj['topProducts'] = topProduct;
+	var uploadValue = document.querySelector('input[type="file"]').value;
+	// var uploadFile = document.querySelector('input[type="file"]').files;
+	obj['uploadedImageSrc'] = uploadValue.slice(12);
+	// console.log(uploadFile);
+
+	console.log(category);
+	console.log(title);
+	console.log(price);
 	console.log(topProduct);
-	var upload = document.querySelector('input[type="file"]').value;
+	console.log(uploadValue.slice(12));
 
-	obj['uploadedImageSrc'] = upload.slice(12);
-	console.log(upload.slice(12));
-	console.log(obj);
-
-	productsArray.push(obj);
-	renderProducts(productsArray);
+	// console.log(obj);
+	if (topProduct === true) {
+		// remove all product cards
+		var miniProductShow = document.querySelectorAll('.mini-product-show');
+		miniProductShow.forEach((element) => {
+			element.style.display = 'none';
+		});
+		alert('added to top products list');
+		topProductsArray.push(obj);
+		renderTopProducts(topProductsArray);
+	} else {
+		// remove all product cards
+		var productcard = document.querySelectorAll('.product-card');
+		productcard.forEach((element) => {
+			element.style.display = 'none';
+		});
+		productsArray.push(obj);
+		renderProducts(productsArray);
+	}
 };
+
+function sortByPrice() {
+	var e = document.getElementById('selectedSort');
+	var category = e.options[e.selectedIndex].value;
+	console.log(category);
+
+	// sortedArray = [];
+	if (category == 1) {
+		priceHighToLow();
+	} else if (category == 2) {
+		priceLowToHigh();
+	} else {
+		console.log(category);
+		// remove all product cards
+		var productcard = document.querySelectorAll('.product-card');
+		productcard.forEach((element) => {
+			element.style.display = 'none';
+		});
+		console.log(productsArray);
+		renderProducts(productsArray);
+	}
+}
+// var newarr =
+function priceLowToHigh() {
+	// remove all product cards
+	var productcard = document.querySelectorAll('.product-card');
+	productcard.forEach((element) => {
+		element.style.display = 'none';
+	});
+
+	if (filterCategory.length !== productsArray.length) {
+		const newarr = filterCategory.sort(function (a, b) {
+			return parseFloat(a.price) - parseFloat(b.price);
+		});
+		renderProducts(newarr);
+	} else {
+		const newarr = productsArray.sort(function (a, b) {
+			return parseFloat(a.price) - parseFloat(b.price);
+		});
+		renderProducts(newarr);
+	}
+
+	// console.log(newarr);
+}
+
+function priceHighToLow() {
+	// remove all product cards
+	var productcard = document.querySelectorAll('.product-card');
+	productcard.forEach((element) => {
+		element.style.display = 'none';
+	});
+
+	if (filterCategory.length !== productsArray.length) {
+		const newarr = filterCategory.sort(function (a, b) {
+			return parseFloat(b.price) - parseFloat(a.price);
+		});
+		renderProducts(newarr);
+	} else {
+		const newarr = productsArray.sort(function (a, b) {
+			return parseFloat(b.price) - parseFloat(a.price);
+		});
+		renderProducts(newarr);
+	}
+}
+
+function rangeSlider(el) {
+	console.log(el.value);
+	var priceRange = document.querySelector('.price-range b span');
+	priceRange.innerHTML = el.value;
+
+	rangeChanged(el.value);
+}
+
+function rangeChanged(val) {
+	// remove all product cards
+	var productcard = document.querySelectorAll('.product-card');
+	productcard.forEach((element) => {
+		element.style.display = 'none';
+	});
+
+	var filterPrice = [];
+	productsArray.forEach((arr) => {
+		if (arr.price < val) {
+			filterPrice.push(arr);
+		}
+	});
+	renderProducts(filterPrice);
+}
+
+var filterCategory = productsArray;
 
 const filterByCategory = (category) => {
 	// remove all product cards
@@ -133,15 +279,14 @@ const filterByCategory = (category) => {
 
 	var SelectedCategoryName = category.textContent;
 
-	var filterCategory = [];
+	var temp = [];
 	productsArray.forEach((arr) => {
 		if (arr.productCategory === SelectedCategoryName) {
-			filterCategory.push(arr);
+			temp.push(arr);
 		}
-		// else if (arr.productCategory === 'Hoodie/T-shirt') {
-		// 	filterCategory.push(arr);
-		// }
 	});
+	filterCategory = temp;
 	console.log(filterCategory);
 	renderProducts(filterCategory);
+	// productsArray = filterCategory;
 };
